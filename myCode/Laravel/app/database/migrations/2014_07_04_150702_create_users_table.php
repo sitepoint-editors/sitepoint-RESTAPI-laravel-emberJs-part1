4e9e7c6648c08d9f13982da9441ea7fb6a733d69
AleksanderKoko <aleksanderkoko@gmail.com>
2014-07-13 14:54:47 +0200

    Update 2014_07_04_150702_create_users_table.php

--- myCode/Laravel/app/database/migrations/2014_07_04_150702_create_users_table.php
+++ myCode/Laravel/app/database/migrations/2014_07_04_150702_create_users_table.php
@@ -14,9 +14,11 @@ class CreateUsersTable extends Migration {
 	{
         Schema::create('users', function($table)
         {
-            $table->increments('id')->unique();
-            $table->string('username');
+            $table->increments('id');
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->string('lastname');
             $table->timestamps();
         });
 	}
